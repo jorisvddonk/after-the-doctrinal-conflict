c9153f24f817ae8957206a8300b342048f3dd4ec
joris <jorisvddonk@gmail.com>
2022-12-29 13:03:50 +0100

    get rid of the other AI ships as they're just getting in the way

--- src/main.gd
+++ src/main.gd
@@ -7,8 +7,8 @@ func _ready():
 	rng.randomize()
 	$PlayerShip.shoot.connect(_on_Ship_shoot.bind($PlayerShip))
 	$PlayerShip.debug.connect(_on_Debug)
-	$Ship.shoot.connect(_on_Ship_shoot.bind($Ship))
-	$Ship2.shoot.connect(_on_Ship_shoot.bind($Ship2))
+	#$Ship.shoot.connect(_on_Ship_shoot.bind($Ship))
+	#$Ship2.shoot.connect(_on_Ship_shoot.bind($Ship2))
 	$Ship3.shoot.connect(_on_Ship_shoot.bind($Ship3))
 
 func _on_Ship_shoot(shotBaseSpeed, targetpos, ship):
@@ -16,11 +16,11 @@ func _on_Ship_shoot(shotBaseSpeed, targetpos, ship):
 	var rot = ship.rotation
 	if targetpos != null:
 		rot = Vector2.UP.angle_to(targetpos - ship.position)
-	bullet.set_velocity(ship.velocity + Vector2(0, -shotBaseSpeed).rotated(rot))
+	bullet.set_velocity((ship.velocity * 1) + Vector2(0, -shotBaseSpeed).rotated(rot))
 	bullet.position = ship.position
 	bullet.rotation = ship.rotation
 	add_child(bullet)
 
 func _on_Debug():
-	$Ship2.rotation = rng.randf_range(-10.0, 10.0)
+	#$Ship2.rotation = rng.randf_range(-10.0, 10.0)
 	$Ship3.rotation = rng.randf_range(-10.0, 10.0)
